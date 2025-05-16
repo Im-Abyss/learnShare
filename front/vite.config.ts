0c9436b394a8dd1bd07e1c5dc8bd9635d5950bcd
Im-Abyss <enty3811@gmail.com>
2025-05-16 19:21:03 +0300

    мелкие доработки в папке front

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-export default defineConfig(
-  {
-    server: {
-      proxy: {
-        '/api': 'http://localhost:8000'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
-)
+})
